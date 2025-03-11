96e635ff3b6ec037cabe19b87fc6b3f7d7c149a9
Agrim Gupta <agrimgupt195@gmail.com>
2025-03-12 02:27:54 +0530

    Fixed naming conventions and add new models and also fix userAuth

--- Backend/app.js
+++ Backend/app.js
@@ -3,12 +3,14 @@ const app = express();
 const cors = require('cors');
 const connectDB = require("./db/db");
 const userRouter = require("./routes/user.routes");
-app.use(cors());//if empty it means it can access any domain 
-connectDB();
-app.get("/", (req, res) => {
-    res.send("Hello World");
-})
+const cookieParser = require("cookie-parser");
 app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    credentials: true,
+}));//if empty it means it can access any domain 
+connectDB();
+
 app.use("/users", userRouter);
 
 module.exports = app    
